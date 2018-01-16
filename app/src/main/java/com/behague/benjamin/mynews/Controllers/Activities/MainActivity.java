437726089897eac0bdda37b0ac3d94a688285485
Benito <behague.benjamin@gmail.com>
2018-01-16 16:05:41 +0100

    MVC

--- app/src/main/java/com/behague/benjamin/mynews/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/behague/benjamin/mynews/Controllers/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.behague.benjamin.mynews;
+package com.behague.benjamin.mynews.Controllers.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.behague.benjamin.mynews.R;
 
 public class MainActivity extends AppCompatActivity {
 
