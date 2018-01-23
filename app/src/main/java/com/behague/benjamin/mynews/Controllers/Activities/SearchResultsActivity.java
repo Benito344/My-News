43d83f7c9f488f1df4afba126d9c17b7240c9e31
Benito <behague.benjamin@gmail.com>
2018-01-23 14:17:25 +0100

    Add click listener on recyclerview for open webviewactivity with the url article selected

--- app/src/main/java/com/behague/benjamin/mynews/Controllers/Activities/SearchResultsActivity.java
+++ app/src/main/java/com/behague/benjamin/mynews/Controllers/Activities/SearchResultsActivity.java
@@ -5,15 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
-import com.behague.benjamin.mynews.Models.SearchArticles.SearchArticlesDoc;
 import com.behague.benjamin.mynews.R;
+import com.behague.benjamin.mynews.Views.ItemClickRecyclerView;
 import com.behague.benjamin.mynews.Views.SearchResultsAdapter;
 import com.bumptech.glide.Glide;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,8 +18,6 @@ import butterknife.ButterKnife;
 public class SearchResultsActivity extends AppCompatActivity {
 
     private SearchResultsAdapter searchResultsAdapter;
-
-    private List<SearchArticlesDoc> searchArticleDoc;
 
     @BindView(R.id.recycler_view_search)
     RecyclerView recyclerView;
@@ -33,18 +28,26 @@ public class SearchResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_results);
         ButterKnife.bind(this);
         this.initRecyclerView();
-        //searchArticleDoc = SearchActivity.searchArticlesDoc;
-        //this.updateList();
+        this.configureOnClickRecyclerView();
     }
 
     public void initRecyclerView(){
-        //this.searchArticleDoc = new ArrayList<>();
         this.searchResultsAdapter = new SearchResultsAdapter(SearchActivity.searchArticlesDoc, Glide.with(this));
         this.recyclerView.setAdapter(this.searchResultsAdapter);
         this.recyclerView.setLayoutManager(new LinearLayoutManager(SearchResultsActivity.this));
     }
 
-    public void updateList(){
-        searchResultsAdapter.notifyDataSetChanged();
+    private void configureOnClickRecyclerView(){
+        ItemClickRecyclerView.addTo(recyclerView, R.layout.fragment_top_stories)
+                .setOnItemClickListener(new ItemClickRecyclerView.OnItemClickListener(){
+                    @Override
+                    public void onItemClicked(RecyclerView recyclerView, int position, View v){
+                        String URL = searchResultsAdapter.getURL(position);
+                        Intent webViewActivity = new Intent(SearchResultsActivity.this, WebViewActivity.class);
+                        webViewActivity.putExtra("URL", URL);
+                        startActivity(webViewActivity);
+                    }
+                });
     }
+
 }
